e519b95b92604081a78a55296ee174fb5451cd56
kamada333 <acrfc333@gmail.com>
2022-07-19 20:21:09 +0900

    lesson9

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
 const PORT = 3000;
+
+// ミドルウェア
+app.use("/api/users", userRoute);
 
 app.get("/", (req, res) => {
   res.send("hello express");
-})
+});
+
+// app.get("/users", (req, res) => {
+//   res.send("users express");
+// });
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
